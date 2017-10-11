de0511bff9f8e0594211bbebbd2227923912bd51
mhackyu <markpaderes0932@yahoo.com>
2017-10-11 22:08:45 +0800

    Token Generator utility added

---
+++ src/AppBundle/Utils/TokenGenerator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhackyu
+ * Date: 10/11/17
+ * Time: 9:43 PM
+ */
+
+namespace AppBundle\Utils;
+
+
+class TokenGenerator
+{
+    public function generateConfirmationToken()
+    {
+        return "mhA" . md5(uniqid()) . "ckYU";
+    }
+}
