6537b162a3f4946bebf76b7131d1c33769725d36
chester <chester.francisco23@gmail.com>
2017-10-12 14:55:56 +0800

    quote category and user module update

--- src/AdminBundle/Controller/QuotesController.php
+++ src/AdminBundle/Controller/QuotesController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Quote;
 use AppBundle\Form\QuoteType;
+use AppBundle\Entity\QuoteCategory;
+use AppBundle\Form\QuoteCategoryType;
 use Symfony\Component\HttpFoundation\Request;
-
 
 class QuotesController extends Controller
 {
@@ -20,7 +21,6 @@ class QuotesController extends Controller
     	$quotes = $em->getRepository('AppBundle:Quote')->findAll();
         return $this->render('AdminBundle:quotes:list.html.twig', [
         	'quotes' => $quotes 
-            // ...
         ]);
     }
 
@@ -78,7 +78,69 @@ class QuotesController extends Controller
         
     }    
 
+     /**
+     * @Route("admin/quotecatg", name="admin_quotecatg_list")
+     */
+    public function listquoteAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $quote_category = $em->getRepository('AppBundle:QuoteCategory')->findAll();
+        return $this->render('AdminBundle:quotes:quotecatg.html.twig', [
+            'quote_category' => $quote_category
+        ]);
+    }
 
+     /**
+     * @Route("admin/quotecatg/new", name="admin_quotecatg_new")
+     */
+    public function newquoteAction(Request $request)
+    {
+        $quote = new QuoteCategory();
+        $form = $this->createForm(QuoteCategoryType::class, $quote);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($quote);
+            $em->flush();
+            return $this->redirectToRoute('admin_quotecatg_list');
+        }
+        return $this->render('AdminBundle:quotes:quotenew.html.twig', [
+                'form'=> $form->createView()
+            ]);
+    }
+
+    /**
+     * @Route("admin/quotecatg/{id}/delete", name="admin_quotecatg_delete")
+     */
+    
+    public function deletequoteAction(QuoteCategory $quotecatg, Request $request)
+    {
+        //TODO: soft deletable
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($quotecatg);
+            $em->flush();
+            return $this->redirectToRoute('admin_quotecatg_list');       
+    }    
+
+     /**
+     * @Route("admin/quotecatg/{id}/edit", name="admin_quotecatg_edit")
+     */
+
+    public function editquoteAction(QuoteCategory $quotecatg, Request $request)
+    {
+        $form = $this->createForm(QuoteCategoryType::class, $quotecatg);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('admin_quotecatg_list');
+        }
+        return $this->render('AdminBundle:quotes:quoteedit.html.twig', [
+                'form'=> $form->createView()
+            ]);
+    }
 
 
 
