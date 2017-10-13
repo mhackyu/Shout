b9a07e31ce751dab0a075d44a5da2c40bd5de6b9
mhackyu <markpaderes0932@yahoo.com>
2017-10-13 12:39:35 +0800

    email confirmation in user registration added

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -20,7 +20,7 @@ class RegistrationController extends Controller
     /**
      * @Route("/register", name="registration")
      */
-    public function registerAction(Request $request)
+    public function registerAction(Request $request, TokenGenerator $tokenGenerator)
     {
         // Prevent user to visit registration page if already logged in
         $authChecker = $this->get('security.authorization_checker');
@@ -40,13 +40,13 @@ class RegistrationController extends Controller
                 ->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
             $user->setRole("ROLE_USER");
+            $user->setConfirmationToken($tokenGenerator->generateConfirmationToken());
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
-            $this->addFlash('success', 'Successfully registered!');
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('registration_check_email', ['u' => $user->getUsername()]);
         }
 
         return $this->render('registration/register.html.twig', [
@@ -57,9 +57,64 @@ class RegistrationController extends Controller
     /**
      * @Route("/check-email", name="registration_check_email")
      */
-    public function checkEmailAction(TokenGenerator $tokenGenerator)
+    public function checkEmailAction(Request $request, \Swift_Mailer $mailer, TokenGenerator $tokenGenerator)
     {
-        dump($tokenGenerator->generateConfirmationToken());
-        return $this->render('registration/check_email.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')
+            ->findOneBy(['username' => $request->get('u')]);
+
+        // This will check if the account is existing.
+        if (!$user) {
+            return $this->redirectToRoute('login');
+        }
+
+        // If already confirmed, redirect to login page.
+        if ($user->isEnabled()) {
+            $this->addFlash('info', 'Account is already activated');
+            return $this->redirectToRoute('login');
+        }
+
+        // Send email confirmation to email of the user.
+        $email = $user->getEmail();
+
+        $message = (new \Swift_Message('Welcome to Shout!'))
+            ->setFrom('contactshoutee@gmail.com','Team Shout')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                    'emails/registration.html.twig', [
+                    'name' => $user->getFirstName(),
+                    'token' => $user->getConfirmationToken()
+                ]),
+                'text/html'
+            );
+
+        $mailer->send($message);
+
+        return $this->render('registration/check_email.html.twig', [
+            'email' => $email,
+            'username' => $user->getUsername()
+        ]);
     }
+
+    /**
+     * @Route("/confirm/{confirmationToken}", name="registration_confirm")
+     */
+    public function confirmAction(User $user)
+    {
+        // This will check if the account is existing.
+        if (!$user) {
+            $this->addFlash('danger', 'Failed to confirm.');
+            return $this->redirectToRoute('login');
+        }
+
+        // Activate user.
+        $em = $this->getDoctrine()->getManager();
+        $user->setEnabled(true);
+        $em->flush();
+        $this->addFlash('success', 'Successfully registered!');
+
+        return $this->redirectToRoute('login');
+    }
+
 }
